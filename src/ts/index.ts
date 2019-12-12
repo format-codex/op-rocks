54c17e4a31ea9b26df0fe8e7371afb509ad06cfd
Duncan Cross <duncan.cross@gmail.com>
2019-12-12 21:45:55 +0000

    Add deterministic, side effect-free and sync flags

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,34 @@
 
-export interface Op<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> {
+export interface BaseOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> {
   perform(ctx: TContext, ...args: TArgs): Promise<TResult>;
 }
+
+export interface DeterministicOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> extends BaseOp<TResult, TContext, TArgs> {
+  isDeterministic: true;
+}
+
+export interface NonDeterministicOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> extends BaseOp<TResult, TContext, TArgs> {
+  isDeterministic?: false;
+}
+
+export interface SideEffectFreeOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> extends BaseOp<TResult, TContext, TArgs> {
+  isSideEffectFree: true;
+}
+
+export interface NonSideEffectFreeOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> extends BaseOp<TResult, TContext, TArgs> {
+  isSideEffectFree?: false;
+}
+
+export interface SyncOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> extends BaseOp<TResult, TContext, TArgs> {
+  isSync: true;
+  performSync(ctx: TContext, ...args: TArgs): TResult;
+}
+
+export interface NonSyncOp<TResult=void, TContext extends object=object, TArgs extends unknown[] = []> extends BaseOp<TResult, TContext, TArgs> {
+  isSync?: false;
+}
+
+export type Op = BaseOp
+  & (DeterministicOp | NonDeterministicOp)
+  & (SideEffectFreeOp | NonSideEffectFreeOp)
+  & (SyncOp | NonSyncOp);
